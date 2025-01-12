59e338b1c597c2f809e4d438bb3bde1e6f4624f0
john04022 <i23018@hb.dhbw-stuttgart.de>
2025-01-12 17:36:36 +0100

    Startmenü test Start.cpp

--- ChainReation/Start.cpp
+++ ChainReation/Start.cpp
@@ -1,17 +1,64 @@
-﻿#include <vector>
+#include <vector>
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include "Spiel.cpp"
 
-int main()
-{
-    std::cout << "Spielfeld erstellen...\n";
-    
-    // TO DO: Pointer mit Smartpointer ersetzten und Klassen/ Methoden schön anordnen, Dateinamen bearbeiten
-    Spiel game = Spiel();
-    game.spielen();
+void zeigeHauptmenue() {
+    std::cout << "Hauptmenü:\n";
+    std::cout << "1. Neues Spiel starten\n";
+    std::cout << "2. Spiel laden\n";
+    std::cout << "Wähle eine Option (1-2): ";
+}
+
+int main() {
+    int auswahl;
+    Spiel game;
+    zeigeHauptmenue();
+    std::cin >> auswahl;
+
+    switch (auswahl) {
+        case 1:
+            std::cout << "Neues Spielfeld wird erstellt...\n";
+            game.spielInitialisieren();
+            break;
+        case 2:
+            std::cout << "Spielstand wird geladen...\n";
+            game = game.spielLaden();
+            break;
+        default:
+            std::cout << "Ungültige Auswahl!\n";
+            return 0;
+    }
+
+    char eingabe;
+    bool spielLaeuft = true;
+
+    game.getSpielfeld().printSpielfeld();
+    while (spielLaeuft) {
+        std::cout << "Drücke 'P' um das Spiel zu pausieren und zu speichern, 'Q' um zu beenden oder 'C' um fortzufahren.\n";
+        std::cin >> eingabe;
+
+        switch (eingabe) {
+            case 'P':
+            case 'p':
+                game.spielSpeichern();
+                std::cout << "Spiel pausiert und gespeichert. Drücke 'C' um fortzufahren.\n";
+                break;
+            case 'Q':
+            case 'q':
+                spielLaeuft = false;
+                break;
+            case 'C':
+            case 'c':
+                game.spielen();
+                break;
+            default:
+                std::cout << "Ungültige Eingabe!\n";
+                break;
+        }
+    }
 
     return 0;
 }
