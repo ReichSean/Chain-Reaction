04e50375a9860e6341f8d1dfd450830ada218dc1
john04022 <i23018@hb.dhbw-stuttgart.de>
2025-01-13 11:06:45 +0100

    Start.cpp Hauptmenü

--- ChainReation/Start.cpp
+++ ChainReation/Start.cpp
@@ -5,8 +5,8 @@
 #include <sstream>
 #include "Spiel.cpp"
 
-void zeigeHauptmenue() {
-    std::cout << "Hauptmenue:\n";
+void hauptmenue() {
+    std::cout << "Willkommen bei ChainReaction!\n";
     std::cout << "1. Neues Spiel starten\n";
     std::cout << "2. Spiel laden\n";
     std::cout << "Waehle eine Option (1-2): ";
@@ -15,51 +15,25 @@ void zeigeHauptmenue() {
 int main() {
     int auswahl;
     Spiel game;
-    zeigeHauptmenue();
+    hauptmenue();
     std::cin >> auswahl;
 
     switch (auswahl) {
         case 1:
             std::cout << "Neues Spielfeld wird erstellt...\n";
             game.spielInitialisieren();
+            game.spielen();
             break;
         case 2:
             std::cout << "Spielstand wird geladen...\n";
             game.spielLaden();
+            game.spielen();
             break;
         default:
             std::cout << "Ungueltige Auswahl!\n";
             return 0;
     }
 
-    char eingabe;
-    bool spielLaeuft = true;
-
-    game.getSpielfeld().printSpielfeld();
-    while (spielLaeuft) {
-        std::cout << "Druecke 'P' um das Spiel zu pausieren und zu speichern, 'Q' um zu beenden oder 'C' um fortzufahren.\n";
-        std::cin >> eingabe;
-
-        switch (eingabe) {
-            case 'P':
-            case 'p':
-                game.spielSpeichern();
-                std::cout << "Spiel pausiert und gespeichert. Druecke 'C' um fortzufahren.\n";
-                break;
-            case 'Q':
-            case 'q':
-                spielLaeuft = false;
-                break;
-            case 'C':
-            case 'c':
-                game.spielen();
-                spielLaeuft = false;
-                break;
-            default:
-                std::cout << "Ungueltige Eingabe!\n";
-                break;
-        }
-    }
 
     return 0;
 }
